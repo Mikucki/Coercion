3cf28c77372d46d654bab90244ef55c8c38ed15d
Mikucki <piotr.mikucki@op.pl>
2022-08-10 14:38:14 -0400

    ex for coercion types :L

---
+++ main.js
@@ -0,0 +1,25 @@
+function findAll(match, array) {
+    let ret = [];
+    for(let i = 0; i < array.length; i++) {
+        if (Object.is(match, v)) {
+            ret.push(v)
+        }
+        else if (match == null && v == null) {
+            ret.push(v)
+        }
+        else if (typeof match == "boolean" && typeof v == "boolean") {
+            if (match == v) {
+                ret.push(v);
+            }
+        }
+        else if (typeof match == "string" && match.trim() != "" && typeof v == "number" && !Object.is(v, -0)){
+            if (match == v){
+                ret.push(v);
+            }
+        }
+        else if(typeof match == "number" && !Object.is(match, NaN) && !Object.is(match, -Infinity) && !Object.is(match, Infinity) && typeof v == "string" && typeof v != NaN){
+
+        }
+    }
+    return ret;
+}
